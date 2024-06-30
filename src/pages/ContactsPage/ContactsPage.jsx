2d94e4fd581a19528bfb475b1cc7e0ad86062968
Niko Pidopryhora <nikolaypidopryhora@gmail.com>
2024-07-01 00:13:51 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,11 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import Loader from "../../components/Loader/Loader";
 import ContactList from "../../components/ContactList/ContactList";
 import { selectError, selectLoading } from "../../redux/contacts/selectors";
+import { fetchContacts } from "../../redux/contacts/operations";
+import { useEffect } from "react";
 
 const styles = {
   container: {
@@ -14,6 +16,11 @@ const styles = {
 export default function ContactPage() {
   const error = useSelector(selectError);
   const loading = useSelector(selectLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div style={styles.container}>
